6f71ec6735d9da401152f2201b3b6dec92bae7cc
stryang <892054096@qq.com>
2021-04-19 15:42:02 +0800

    update：修改mybatis配置

--- src/main/java/com/stg/gateway/config/MybatisPlusConfig.java
+++ src/main/java/com/stg/gateway/config/MybatisPlusConfig.java
@@ -1,6 +1,10 @@
 package com.stg.gateway.config;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +13,17 @@ import org.springframework.context.annotation.Configuration;
  * @date 2021-04-19 9:42
  */
 @Configuration
+@MapperScan("com.stg.gateway.*.mapper*")
 public class MybatisPlusConfig {
 
     /**
      * 分页插件
      */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return mybatisPlusInterceptor;
     }
 }
